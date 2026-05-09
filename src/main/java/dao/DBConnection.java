d8b71138d2b356dc4d5f943952064b881b0db3a0
Phan Uyen <admin@Uyen.local>
2026-05-09 12:58:12 +0700

    sửa dbconnect

--- src/main/java/dao/DBConnection.java
+++ src/main/java/dao/DBConnection.java
@@ -4,10 +4,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBConnection {
-    // Thay "root" và "123456" bằng username và password MySQL của máy bạn
     private static final String URL = "jdbc:mysql://localhost:3306/ban_dien_thoai?useUnicode=true&characterEncoding=UTF-8";
     private static final String USER = "root";
-    private static final String PASS = "123456"; 
+    private static final String PASS = "123456789"; 
 
     public static Connection getConnection() {
         try {

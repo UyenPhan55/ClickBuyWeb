922e4fc326c96af5d3febbd70c5537ce8f851ca0
Phan Uyen <admin@Uyen.local>
2026-05-09 13:00:18 +0700

    sửa dbconnect

--- src/main/java/dao/DBConnection.java
+++ src/main/java/dao/DBConnection.java
@@ -4,16 +4,18 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/ban_dien_thoai?useUnicode=true&characterEncoding=UTF-8";
+
+    private static final String URL =
+        "jdbc:mysql://localhost:3306/clickbuy" +
+        "?useUnicode=true&characterEncoding=UTF-8&serverTimezone=Asia/Ho_Chi_Minh";
     private static final String USER = "root";
     private static final String PASS = "123456789"; 
-
     public static Connection getConnection() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             return DriverManager.getConnection(URL, USER, PASS);
         } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println("LỖI KẾT NỐI DB: " + e.getMessage());
             return null;
         }
     }
